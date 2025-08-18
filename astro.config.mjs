95900de2995245879af807ca42acdf6003d99541
dorianvp <dorianvp@zingolabs.org>
2025-08-18 16:13:48 -0300

    feat: gh pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	site: "https://zingolabs.github.io",
+	base: "/",
 	integrations: [mdx(), sitemap(), react()],
 
 	vite: {
