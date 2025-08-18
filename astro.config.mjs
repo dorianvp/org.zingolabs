629c046264822d2321c182c3f9e443435e0f0c78
dorianvp <dorianvp@zingolabs.org>
2025-08-18 15:08:28 -0300

    chore: remove vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,20 +7,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel";
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap(), react()],
+	site: "https://example.com",
+	integrations: [mdx(), sitemap(), react()],
 
-  vite: {
-      plugins: [tailwindcss()],
+	vite: {
+		plugins: [tailwindcss()],
 	},
 
-  redirects: {
-      "/": "/zingo",
+	redirects: {
+		"/": "/zingo",
 	},
-
-  adapter: vercel(),
 });
